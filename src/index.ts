0d02b1388116477fc820e90250c2c0524f2417a4
Kanom99life <teeraphat.firstarz@gmail.com>
2024-04-04 14:56:08 +0700

    first.cm

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,35 @@
-console.log("typescript!")
+import {CarRanking} from './test/exercises.test';
+
+export function sortCarsByScore(cars: CarRanking[]): string[] {
+    return [...cars]
+        .sort((i: CarRanking, j: CarRanking) => {
+            if (i.score > j.score) return -1;
+            else return 1;
+        })
+        .map((car) => car.name);
+}
+
+export function sortedCarbyYear(cars: CarRanking[]): string[] {
+    return [...cars]
+        .filter((car) => car.year === 2021)
+        .map((car) => car.name);
+}
+
+
+export function maxAvgPriceCar(cars: CarRanking[]) :string{
+    return [...cars]
+        .map((car) => ({ name: car.name, avgPrice: (car.priceRange[0] + car.priceRange[1]) / 2 }))
+        .sort((i, j) => {if (i.avgPrice > j.avgPrice) return -1; else return 1})[0].name;
+}
+
+interface groupedYear{
+    [key: number]: string[];
+  }
+
+export function groupedCarNamesByYear(cars: CarRanking[]): groupedYear {
+    return [...cars]
+        .reduce((acc: groupedYear, car: CarRanking) => {
+        acc[car.year] ? acc[car.year].push(car.name) : acc[car.year] = [car.name];
+        return acc;
+    }, {});
+}
--- src/test/exercises.test.ts
+++ src/test/exercises.test.ts
@@ -1,4 +1,6 @@
-type CarRanking = {
+import { sortCarsByScore, sortedCarbyYear, maxAvgPriceCar, groupedCarNamesByYear } from "../index";
+
+export type CarRanking = {
   name: string;
   year: number;
   score: number;
@@ -70,7 +72,12 @@ const cars: CarRanking[] = [
 
 describe("should produce right answers", () => {
   test("1: should produce sorted car names by score in ascending order", () => {
-    expect(cars).toStrictEqual([
+    
+    // let x = [...cars].sort((i:CarRanking, j:CarRanking) => {
+    //   if (i.score > j.score) return -1; else return 1}).map((car) => car.name);
+    
+    let sortedCarNames = sortCarsByScore([...cars]);
+    expect(sortedCarNames).toStrictEqual([
       "Honda Civic",
       "Mazda Mazda3",
       "Volkswagen Golf",
@@ -82,18 +89,61 @@ describe("should produce right answers", () => {
       "Toyota Prius",
       "Mitsubishi Mirage"
     ]);
+
   });
 
   test("2: should return 2021 car names", () => {
-    expect(cars).toStrictEqual(["Kia Rio", "Volkswagen Golf", "MINI Cooper", "Mitsubishi Mirage", "Mazda Mazda3"]);
+    // let x = [...cars].filter((i) => i.year === 2021).map((car) => car.name);
+    let sorted2021CarNames = sortedCarbyYear([...cars]);
+    expect(sorted2021CarNames).toStrictEqual(["Kia Rio", "Volkswagen Golf", "MINI Cooper", "Mitsubishi Mirage", "Mazda Mazda3"]);
   });
 
   test("3: should return car that has highest average car price", () => {
-    expect(cars).toStrictEqual("MINI Cooper");
+   
+    // let x = [...cars].map((car) => ({ name: car.name, avgPrice: car.priceRange.reduce((a, b) => a + b, 0) / 2}))
+    // .sort((i, j) => {if (i.avgPrice > j.avgPrice) return -1; else return 1})[0].name;
+
+    let maxAvgPriceCarName = maxAvgPriceCar([...cars]);
+    expect(maxAvgPriceCarName).toStrictEqual("MINI Cooper");
   });
 
   test("4: should return grouped car names by year", () => {
-    expect(cars).toStrictEqual({
+    let groupCarNamesByYear = groupedCarNamesByYear([...cars]);
+    // interface groupedYear{
+    //   [key: number]: string[];
+    // }
+
+    //Shorthand
+    // let x = [...cars].reduce((acc:groupedYear, car: CarRanking) => {
+    //   acc[car.year] ? acc[car.year].push(car.name) : acc[car.year] = [car.name]
+    //   return acc;
+    // }, {});
+     
+    // Youtube method
+    // let x = [...cars].reduce((acc: groupedYear, car: CarRanking) => {
+    //   const key = car.year;
+    //   acc[key] = acc[key] || [];
+    //   acc[key].push(car.name);
+    //   return acc;
+    // }, {} as groupedYear);
+    
+    // If/else method
+    // Not use interface
+    // let x = [...cars].reduce((acc:{[year: number]: string[]}, car: CarRanking) => {
+    
+     //use interface (gruopedYear)
+    // let x = [...cars].reduce((acc:groupedYear, car: CarRanking) => {
+    //   if (acc[car.year]){
+    //     acc[car.year].push(car.name)
+    //   }
+    //   else{
+    //     acc[car.year] = [car.name]
+    //   }
+    //   return acc;
+    // }, {});
+
+
+    expect(groupCarNamesByYear).toStrictEqual({
       2020: ["Chevrolet Sonic"],
       2021: ["Kia Rio", "Volkswagen Golf", "MINI Cooper", "Mitsubishi Mirage", "Mazda Mazda3"],
       2022: ["Honda Civic", "Toyota Corolla", "Subaru Impreza", "Toyota Prius"]
